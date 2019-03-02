67380eda378ea734c710d27091b4308a0be4e609
philgagnon12 <philgagnon12@gmail.com>
2019-03-02 00:32:28 -0500

    Added vscode.json

--- install.bat
+++ install.bat
@@ -1,3 +1,6 @@
-n:: AutoHotKey
+:: AutoHotKey
+mv %HOMEPATH%\Documents\AutoHotkeyU64.ahk %HOMEPATH%\Documents\_AutoHotkeyU64.ahk
 mklink /H %HOMEPATH%\Documents\AutoHotkeyU64.ahk autohotkey-windows-mac-keyboard\MacKeyboard.ahk
-
+:: VS Code
+mv %APPDATA%\Code\User\settings.json %APPDATA%\Code\User\_settings.json
+mklink /H %APPDATA%\Code\User\settings.json vscode.json
