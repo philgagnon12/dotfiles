e10adb6b4584f96a2833d8e853703ca846ede7bc
philgagnon12 <philgagnon12@gmail.com>
2019-03-02 00:44:17 -0500

    Added up and down dir

--- install.bat
+++ install.bat
@@ -2,5 +2,5 @@
 mv %HOMEPATH%\Documents\AutoHotkeyU64.ahk %HOMEPATH%\Documents\_AutoHotkeyU64.ahk
 mklink /H %HOMEPATH%\Documents\AutoHotkeyU64.ahk autohotkey-windows-mac-keyboard\MacKeyboard.ahk
 :: VS Code
-mv %APPDATA%\Code\User\settings.json %APPDATA%\Code\User\_settings.json
+mv %APPDATA%\Code\User\settings.json
 mklink /H %APPDATA%\Code\User\settings.json vscode.json
