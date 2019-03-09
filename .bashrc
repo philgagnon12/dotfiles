257562cf583a6fe88bfd1567aa973b22774ab078
philgagnon12 <philgagnon12@gmail.com>
2019-03-09 14:30:31 +0000

    Removed vim-multiple-cursor Linking in install.sh

--- .bashrc
+++ .bashrc
@@ -1,9 +1,16 @@
+#Enable colors
 source .dotfiles/bash_colors.sh
 export CLI_COLORS=1
 export PS1="$(clr_red \\u):$(clr_cyan \\W)\$ "
 
 #export LS_COLORS
 dircolors > /dev/null
+
+# VIM as default editor
+export EDITOR=vim
+
+# GIT powerline
+source .dotfiles/bash-powerline.sh
 
 #include aliases
 source .dotfiles/aliases.sh
--- install.sh
+++ install.sh
@@ -1,5 +1,22 @@
-mkdir ~/.dotfiles
-cp -R * ~/.dotfiles
-# TODO append
-cp .bashrc ~/
-cp .tmux.conf ~/
+#!/bin/bash
+
+# Directory of this script
+DIR=dirname $0
+
+# Create destination
+DOTFILES=~/.dotfiles
+mkdir ${DOTFILES}
+
+# Symbolic links
+ln -s ${DIR}/aliases.sh ${DOTFILES}
+ln -s ${DIR}/bash_colors.sh ${DOTFILES}
+ln -s ${DIR}/bash-powerline.sh ${DOTFILES}
+ln -s ${DIR}/pain_control.tmux ${DOTFILES}
+ln -s ${DIR}/sensible.tmux ${DOTFILES}
+
+# Move to not overwrite
+mv ~/.bashrc ~/._bashrc
+cp ~/.tmux.conf ~/._tmux.conf
+
+ln -s ${DIR}/.bashrc ~
+ln -s ${DIR}/.tmux.conf ~
