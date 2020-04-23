49b69b3fa4a6e232a320086607861b15d5f35a16
philgagnon12 <philgagnon12@hotmail.com>
2020-04-23 15:45:06 -0400

    oksh profile

--- aliases.sh
+++ aliases.sh
@@ -1,5 +1,5 @@
 #Colored ls
-ls --color=auto 2>/dev/null
+ls --color=auto > /dev/null 2>&1
 if [[ $? -eq 1 ]]; then alias ls='ls -G'; else alias ls='ls --color=auto'; fi
 alias grep="grep --color=auto"
 alias egrep="egrep --color=auto"
